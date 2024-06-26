83725fdce79a5e9b4c9089da974121f010144609
Lucas Israel <lisraelalves92@gmail.com>
2024-06-26 18:42:10 -0300

    req07

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -1,13 +1,31 @@
 package com.betrybe.sociallogin
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
 
-    // init
+    private val email: TextInputLayout by lazy { findViewById(R.id.email_text_input_layout) }
+    private val pass: TextInputLayout by lazy { findViewById(R.id.password_text_input_layout) }
+    private val loginBtn: Button by lazy { findViewById(R.id.login_button) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loginBtnEnabler()
+
     }
+
+    private fun loginBtnEnabler() {
+        email.editText?.addTextChangedListener {
+            loginBtn.isEnabled = !email.editText?.text.isNullOrEmpty() && !pass.editText?.text.isNullOrEmpty()
+        }
+        pass.editText?.addTextChangedListener {
+            loginBtn.isEnabled = !email.editText?.text.isNullOrEmpty() && !pass.editText?.text.isNullOrEmpty()
+        }
+    }
+
 }
