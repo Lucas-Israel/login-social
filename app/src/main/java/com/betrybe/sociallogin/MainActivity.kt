1129ebcc4a2bc1d9a3b049aa39d7ba673b83d7cf
Lucas Israel <lisraelalves92@gmail.com>
2024-06-27 13:17:31 -0300

    removendo repetição do codigo

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.betrybe.sociallogin
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import com.google.android.material.textfield.TextInputLayout
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private val email: TextInputLayout by lazy { findViewById(R.id.email_text_input_layout) }
     private val pass: TextInputLayout by lazy { findViewById(R.id.password_text_input_layout) }
@@ -15,17 +16,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        loginBtnEnabler()
+        email.editText?.addTextChangedListener { loginBtnEnabler() }
+        pass.editText?.addTextChangedListener { loginBtnEnabler() }
+        loginBtn.setOnClickListener(this)
 
     }
 
     private fun loginBtnEnabler() {
-        email.editText?.addTextChangedListener {
-            loginBtn.isEnabled = !email.editText?.text.isNullOrEmpty() && !pass.editText?.text.isNullOrEmpty()
-        }
-        pass.editText?.addTextChangedListener {
-            loginBtn.isEnabled = !email.editText?.text.isNullOrEmpty() && !pass.editText?.text.isNullOrEmpty()
-        }
+        loginBtn.isEnabled = !email.editText?.text.isNullOrEmpty() && !pass.editText?.text.isNullOrEmpty()
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
 
 }
