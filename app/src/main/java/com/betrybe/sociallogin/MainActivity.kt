3fb7217c8d9fa22d3da93df3f3aeeb5acedf0242
Lucas Israel <lisraelalves92@gmail.com>
2024-06-27 13:44:00 -0300

    req08

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.betrybe.sociallogin
 
 import android.os.Bundle
+import android.util.Patterns
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import com.google.android.material.textfield.TextInputLayout
@@ -16,18 +18,35 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // email and password listener
         email.editText?.addTextChangedListener { loginBtnEnabler() }
         pass.editText?.addTextChangedListener { loginBtnEnabler() }
+
+        // login button logic
         loginBtn.setOnClickListener(this)
 
     }
 
+    // function for enabling login button
     private fun loginBtnEnabler() {
+        // to clear email error if there is one.
+        if (email.error != null) email.error = null
+        // if email and password field are empty, button remains disabled
         loginBtn.isEnabled = !email.editText?.text.isNullOrEmpty() && !pass.editText?.text.isNullOrEmpty()
     }
 
-    override fun onClick(p0: View?) {
-        TODO("Not yet implemented")
+    override fun onClick(v: View?) {
+
+        when (v?.id) {
+            // Email validation
+            R.id.login_button -> {
+                val emailText = email.editText?.text.toString()
+                val isEmailValid: Boolean = Patterns.EMAIL_ADDRESS.matcher(emailText).matches()
+                if (!isEmailValid) {
+                    email.error = getString(R.string.email_warning)
+                }
+            }
+        }
     }
 
 }
